6fd6f2649ba882cc8aee02b4f7715a0dbbfd84b6
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-02-27 11:14:41 -0600

    File -> New Project
    Setup a simple page with 400 images

---
+++ HelloGlide/HelloGlide/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloGlide
+{
+	public partial class MainPage : ContentPage
+	{
+		readonly Uri uri = new Uri ("https://pbs.twimg.com/media/DZ_B8cEU8AASnUI.jpg");
+
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+			for (int i = 0; i < 100; i++) {
+				_grid.RowDefinitions.Add (new RowDefinition { Height = 100 });
+
+				for (int j = 0; j < 4; j++) {
+					var image = new Image {
+						Source = ImageSource.FromUri (uri)
+					};
+					Grid.SetRow (image, i);
+					Grid.SetColumn (image, j);
+					_grid.Children.Add (image);
+				}
+			}
+		}
+	}
+}
